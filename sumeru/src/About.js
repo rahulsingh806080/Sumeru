3d9a5b9304115f36c66cad86230a07a980aac767
rahul singh <rahulps.singh1@gmail.com>
2020-07-27 20:04:45 +0530

    refactoring and about us page done

--- sumeru/src/About.js
+++ sumeru/src/About.js
@@ -1,8 +1,16 @@
 import React from "react";
+import web from "../src/images/contact.jpg";
+import { NavLink } from "react-router-dom";
+import Common from "./Common";
 const About = () => {
   return (
     <>
-      <h1>About</h1>
+      <Common
+        name="Welcome to About Page"
+        imgsrc={web}
+        visit="/contact"
+        btname="Contact now"
+      ></Common>
     </>
   );
 };
--- sumeru/src/Home.js
+++ sumeru/src/Home.js
@@ -1,36 +1,16 @@
 import React from "react";
 import web from "../src/images/web.png";
 import { NavLink } from "react-router-dom";
+import Common from "./Common";
 const Home = () => {
   return (
     <>
-      <section id="header" className="d-flex align-items-center">
-        <div className="container-fluid nav_bg">
-          <div className="row">
-            <div className="col-10 mx-auto">
-              <div className="row">
-                <div className="col-md-6 pt-5 pt-lg-0 order-2 order-lg-1 d-flex justify-content-center flex-column">
-                  <h1>
-                    Grow your business with
-                    <strong className="brand-name"> MatriMeru Tech</strong>
-                  </h1>
-                  <h2 className="my-3">
-                    We are the team of talented developer for making websites.
-                  </h2>
-                  <div className="mt-3">
-                    <NavLink to="/service" className="btn-get-started">
-                      Get Started
-                    </NavLink>
-                  </div>
-                </div>
-                <div className="col-lg-6 order-1 order-2 header-img">
-                  <img src={web} className="img-fluid animated" alt="homeimg" />
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
+      <Common
+        name="Grow your business with"
+        imgsrc={web}
+        visit="/service"
+        btname="Get Started"
+      ></Common>
     </>
   );
 };
