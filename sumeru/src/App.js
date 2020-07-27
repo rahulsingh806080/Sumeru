c482d2d2ff4b4de1f643d9734052c8b1703ad1d0
rahul singh <rahulps.singh1@gmail.com>
2020-07-27 18:10:46 +0530

    Navbar component added

--- sumeru/src/App.js
+++ sumeru/src/App.js
@@ -1,15 +1,19 @@
 import React from "react";
 // import logo from "./logo.svg";
 import { Switch, Route, Redirect } from "react-router-dom";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
 import Home from "./Home";
 import About from "./About";
 import Contact from "./Contact";
 import Service from "./Service";
+import Navbar from "./Navbar";
 import "./App.css";
 
 const App = () => {
   return (
     <>
+      <Navbar></Navbar>
       <Switch>
         <Route exact path="/" component={Home}></Route>
         <Route exact path="/about" component={About}></Route>
