b205145c1259174a315d9e8771aaa529decd3e83
rahul singh <rahulps.singh1@gmail.com>
2020-07-27 17:49:42 +0530

    Routing addeded for pages

--- sumeru/src/App.js
+++ sumeru/src/App.js
@@ -1,11 +1,22 @@
 import React from "react";
 // import logo from "./logo.svg";
+import { Switch, Route, Redirect } from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
+import Contact from "./Contact";
+import Service from "./Service";
 import "./App.css";
 
 const App = () => {
   return (
     <>
-      <h1>Welcome</h1>
+      <Switch>
+        <Route exact path="/" component={Home}></Route>
+        <Route exact path="/about" component={About}></Route>
+        <Route exact path="/service" component={Service}></Route>
+        <Route exact path="/contact" component={Contact}></Route>
+        <Redirect to="/"></Redirect>
+      </Switch>
     </>
   );
 };
